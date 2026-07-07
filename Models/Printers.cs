e39c95a6c1cca52d67cccdf57256706c0bc6929c
Solomon Huckstep <Solomon.Huckstep@hbgusa.com>
2026-07-07 13:19:22 -0400

    File is not renamed to completed like different setups so trying this

--- Models/Printers.cs
+++ Models/Printers.cs
@@ -52,21 +52,8 @@ public class Printer
         _copWatcher = new FileSystemWatcher(copPath) { EnableRaisingEvents = true };
         _popWatcher = new FileSystemWatcher(popPath) { EnableRaisingEvents = true };
 
-        _copWatcher.Error += OnWatcherError;
-        _popWatcher.Error += OnWatcherError;
-
         _copWatcher.Renamed += OnFileRenamed;
         _popWatcher.Renamed += OnFileRenamed;
-    }
-
-    private void OnWatcherError(object s, ErrorEventArgs e)
-    {
-        var watcher = (FileSystemWatcher)s;
-
-        Messages.Warning($"{e}");
-
-        watcher.EnableRaisingEvents = false;
-        watcher.EnableRaisingEvents = true;
     }
 
     private List<LabelFile> GetTargetQueue(PrinterQueue queue) =>
@@ -97,7 +84,7 @@ public class Printer
             }
         }
 
-        if (ext == ".COMPLETED")
+        if (ext == ".COMPLETED" || e.FullPath.Contains("completed"))
         {
             lock (_lock)
             {
